d9e358c13c1cdbf6f80d3d99d4da132538de960c
Taegu <gmdals45@gmail.com>
2025-08-10 16:55:41 +0900

    UPDATE Practical question 1, 2

--- sqlp/실기문제1.sql
+++ sqlp/실기문제1.sql
@@ -1,14 +1,20 @@
-
 SELECT parameter, value
 FROM v$option
 WHERE parameter = 'Partitioning';
+
+/*
+
+ * Enterprise 버전이 아니라서 파티셔닝 부분은 제외 
+
+*/
 
 CREATE TABLE 실기1_주문 (
     주문번호 NUMBER NOT NULL
     , 주문일시 DATE NOT NULL
     , 주문유형코드 VARCHAR2(10) NOT NULL
-    , CONSTRAINT 실기1_주문_PK PRIMARY KEY (주문번호) USING INDEX LOCAL
-)
+    , CONSTRAINT 실기1_주문_PK PRIMARY KEY (주문번호) -- USING INDEX LOCAL
+) ;
+
 PARTITION BY RANGE (주문번호) (
     PARTITION P_202201 VALUES LESS THAN ('20220201')
     , PARTITION P_MAXVALUE VALUES LESS THAN (MAXVALUE)
@@ -18,8 +24,9 @@ CREATE TABLE 실기1_주문상세 (
     주문번호 VARCHAR2(16) NOT NULL
     , 주문상품코드 VARCHAR2(10) NOT NULL
     , 주문수량 NUMBER NOT NULL 
-    , CONSTRAINT 실기1_주문상세_PK PRIMARY KEY (주문번호, 주문상품코드) USING INDEX LOCAL
-) 
+    , CONSTRAINT 실기1_주문상세_PK PRIMARY KEY (주문번호, 주문상품코드) -- USING INDEX LOCAL
+) ;
+
 PARTITION BY RANGE (주문번호) ( 
     PARTITION P_202201 VALUE LESS THAN ('20220201')
     , PARTITION P_MAXVALUE VALUE LESS THAN (MAXVALUE) 
@@ -32,9 +39,15 @@ CREATE TABLE 실기1_상품 (
     , CONSTRAINT 실기1_상품_PK PRIMARY KEY (상품코드)
 );
 
-CREATE INDEX 실기1_주문_X1 ON 실기1_주문 (주문일시, 주문번호) LOCAL; 
+CREATE INDEX 실기1_주문_X1 ON 실기1_주문 (주문일시, 주문번호); -- LOCAL; 
 
-/**문제**/
+commit;
+
+/*
+ 문제
+  1. 지문의 SQL을 변경하고 힌트를 기술하라. (단, 힌트는 조인방식과 조인순서에 대한 힌트만 기술)
+  2. 병렬실행은 고려하지 않음 
+*/
 SELECT a.주문일시
     , b.상품명
     , a.주문수량_A01
@@ -49,25 +62,27 @@ FROM (SELECT TRUNC (a.주문일시) AS 주문일시
             AND a.주문일시 < TO_DATE ('2022-02-01', 'YYYY-MM-DD')
             AND b.주문번호 = a.주문번호
         GROUP BY TRUNC (a.주문일시), b.주문상품코드) a
-        , 상품 b 
+        , 실기1_상품 b 
 WHERE b.상품코드 = a.주문상품코드
     AND b.상품유형코드 = 'A01'
 ;
 
 /**
 
-1. 파티션 Pruning을 위해 주문일시 조건을 주문번호로 변경한다 
+1. 파티션 Pruning을 위해 주문일시 조건을 주문번호로 변경한다
+ ㅇ 파티션 프루닝은 쿼리를 실행할 때 필요한 파티션만 읽고, 나머지 파티션은 건너뛰는 최적화 기법 
 2. 그룹핑 전에 상품 테이블을 조인하여 그룹핑 대상을 감소시킨다
-3. TRUNC (c.주문일시), c.주문유형코드, a.상품코드, a.상품명로 그룹핑을 먼저 수행하여 NVL, DECODE 함수의 수행 횟수를 감소시킨다 
+3. TRUNC (c.주문일시), c.주문유형코드, a.상품코드, a.상품명로 그룹핑을 먼저 수행하여, NVL, DECODE 함수의 수행 횟수를 감소시킨다 
 4. SUM 함수 내부에 사용한 NVL 함수를 집계가 끝난 후 1번만 수행할 수 있게 사용 위치를 변경한다 
 
 **/
-SELECT 주문일자,
+SELECT 주문일자
     , 상품코드
     , 상품명
-    , NVL (SUM (DECODE (a.주문유형코드, 'A01', NVL (b.주문수량, 0))), 0) AS 주문수량_A01 -- 4.
-    , NVL (SUM (DECODE (a.주문유형코드, 'A02', NVL (b.주문수량, 0))), 0) AS 주문수량_A02 -- 4. 
-FROM (SELECT TRUNC (a.주문일시) AS 주문일자
+    , NVL (SUM (DECODE (주문유형코드, 'A01', 주문수량)), 0) AS 주문수량_A01 -- 4.
+    , NVL (SUM (DECODE (주문유형코드, 'A02', 주문수량)), 0) AS 주문수량_A02 -- 4. 
+FROM (SELECT /*+ LEADING(A B C) USE_HASH(A B C) */ 
+         TRUNC (c.주문일시) AS 주문일자
         , c.주문유형코드
         , a.상품코드
         , a.상품명 
@@ -76,15 +91,14 @@ FROM (SELECT TRUNC (a.주문일시) AS 주문일자
             , 실기1_주문상세 b
             , 실기1_주문 c 
         WHERE a.상품유형코드 = 'A01'
-            AND  b.주문상품코드 = a.상품코드
-            AND a.주문번호 >= TO_DATE ('2022-01-01', 'YYYY-MM-DD') -- 1.
-            AND a.주문번호 < TO_DATE ('2022-02-01', 'YYYY-MM-DD') -- 1.
-            AND b.주문번호 = a.주문번호
+            AND b.주문상품코드 = a.상품코드
+            AND b.주문번호 >= '20220101' -- 1.
+            AND b.주문번호 < '20220201' -- 1.
+            AND c.주문번호 = b.주문번호
         GROUP BY TRUNC (c.주문일시), c.주문유형코드, a.상품코드, a.상품명) -- 3. 
 GROUP BY 주문일자
     , 상품코드
     , 상품명 
 ;
-
 
 
--- sqlp/실기문제2.sql
+++ sqlp/실기문제2.sql
@@ -20,7 +20,12 @@ CREATE TABLE 실기2_고객 (
 
 CREATE INDEX 실기2_주문_X1 ON 실기2_주문 (주문일시, 주문번호); 
 
--- 문제
+commit; 
+
+/*
+ 문제
+  1. 지문의 SQL을 변경하고 힌트를 기술하라. (단, 힌트는 조인방식과 조인순서에 대한 힌트만 기술)
+*/
 SELECT 
     *
 FROM (SELECT a.*,
@@ -43,17 +48,25 @@ FROM (SELECT a.*,
                 ORDER BY 1) a)
 WHERE rn BETWEEN 101 AND 200;
 
--- 해설
-SELECT /*+ LEADING(a b) use_nl(a b) */ -- 4.
+/*
+
+    1. ORDER BY A.주문일시로 변경  
+     ㅇ ORDER BY 1에 의해 to_char(a.주문일시, 'YYYY-MM-DD HH24:MI:SS') 표현식으로 데이터를 정렬하여 소트에 인덱스를 활용할 수 없으므로
+    2. ROWNUM <= 200 조건절 추가 
+     ㅇ ROWNUM 슈도 컬럼을 직접 기술하지 않아 부분 범위 처리가 불가능
+    3. 고객 테이블의 조인 횟수를 감소시키기 위해 Top-N 처리 후 고객 테이블을 조인
+    4. 주문상세 테이블은 Top-N 처리에서는 인덱스만 액세스하고 Top-N 처리 후 셀프 조인을 통해 나머지 컬럼 조회
+*/
+SELECT /*+ LEADING(A B) USE_NL(A B) */ 
     a.*
     ,b.주문수량
     ,c.고객명
     FROM (SELECT a.*
             , ROWNUM as rn
-        FROM (SELECT /*+ LEADING(a b) use_nl(a b) */ -- 4.
+        FROM (SELECT /*+ LEADING(A B) USE_NL(A B) */ 
                     TO_CHAR (a.주문일시, 'YYYY-MM-DD HH24:MI:SS') AS 주문일시
                     , a.주문고객번호
-                    , b.ROWID as rid -- 5. 
+                    , b.ROWID as rid -- 4
             FROM 실기2_주문 a
                 , 실기2_주문상세 b                
             WHERE a.주문일시 >= TO_DATE ('2022-01-01', 'YYYY-MM-DD')
@@ -65,7 +78,7 @@ SELECT /*+ LEADING(a b) use_nl(a b) */ -- 4.
     , 실기2_주문상세 b 
     , 실기2_고객 c
 WHERE rn >= 101
-AND b.ROWID = a.rid -- 5.
-AND c.고객번호 = a.주문고객번호 -- 3. 
-ORDER BY a.주문일시
+AND b.ROWID = a.rid -- 4
+AND c.고객번호 = a.주문고객번호 -- 3
+ORDER BY a.주문일시 -- 1. 
 ;
